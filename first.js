7721fc082e2f7426503304bb0d635102ca2bc030
hyannu <annurao9560@gmail.com>
2026-09-23 06:29:57 +0530

    if else,else if statement

--- first.js
+++ first.js
@@ -1,20 +1,63 @@
 //conditional statements
-// let age=45;
-// if(age>=18){
-//     console.log("You can voat");
+let age=45;
+if(age>=18){
+    console.log("You can voat");
 
-// }else{
-//     console.log("You cantnot voat");
-// }
+}else{
+    console.log("You cantnot voat");
+}
 
 
 
 // odd or even number
 
-let n=20;
-if(n % 2 ===0){
-    console.log(n,"is even");
+// let n=20;
+// if(n % 2 ===0){
+//     console.log(n,"is even");
 
+// }else{
+//     console.log(n,"is odd");
+// }
+
+
+// let age=25;
+// let result=age>= 18 ? "adult" : "not adult";
+// console.log(result); 
+
+// let age=2;
+// let result=age>= 18 ? console.log ( "adult" ): console.log ("not adult");
+
+
+// alert("hello alert ho jao !");
+
+// let name=prompt("hello deepak");
+// console.log(name);
+
+
+
+/*prectice question [1.]..........  
+
+let num=prompt("enter any number");
+
+if(num % 5===0){
+    console.log(num,"is a multiple of 5");
 }else{
-    console.log(n,"is odd");
+    console.log(num, "is a not multiple of 5");
 }
+    */
+
+
+//prectice question [2.]..............
+/*let marks=99;
+if(marks>=90 && marks <=100){
+    console.log("A");
+}else if(marks >=70 && marks <=89){
+    console.log("B");
+}else if (marks >=60 && marks <= 69){
+    console.log("C");
+}else if(marks >=50 && marks <=59){
+    console.log("D");
+}else{
+    console.log("fail");
+}
+    */
